a73984120c6943a8cac4306f7a72bea98faeb8a6
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2018-02-09 15:38:29 -0500

    Login: Add basic Login layout
    Designed a simple login layout with a username and password text fields,
    with the password text field masking the entered text
    
    The 'cancel' button will return back to the Welcome (Start) Activity,
    while the 'login' button does not currently do anything

--- app/src/main/java/edu/gatech/buzzshelter/controller/LoginActivity.java
+++ app/src/main/java/edu/gatech/buzzshelter/controller/LoginActivity.java
@@ -1,8 +1,10 @@
 package edu.gatech.buzzshelter.controller;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.Button;
 
 import edu.gatech.buzzshelter.R;
 
@@ -15,6 +17,20 @@ public class LoginActivity extends AppCompatActivity
         setContentView(R.layout.activity_login);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Button login = findViewById(R.id.loginButton);
+        Button cancel = findViewById(R.id.cancelButton);
+
+        /* Implement cancel feature */
+        cancel.setOnClickListener(v -> {
+            Intent back = new Intent(this, WelcomeActivity.class);
+            startActivity(back);
+        });
+
+        /* Implement login feature */
+        login.setOnClickListener(v -> {
+            /* TODO */
+        });
     }
 
 }
