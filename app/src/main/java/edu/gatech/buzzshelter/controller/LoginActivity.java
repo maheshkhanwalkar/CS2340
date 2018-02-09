de5369d874ac4ff8479c172b2e0c941fe73d5425
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2018-02-09 16:18:45 -0500

    Login: Implement hardcoded credential checking
    When 'login' is pressed on the Login page, the username and password
    fields are checked against a hard-coded value, which either passes or
    fails:
    
    1. On Pass: TODO (doesn't do anything right now)
    2. On Fail: Display error Snackbar and clear the password field

--- app/src/main/java/edu/gatech/buzzshelter/controller/LoginActivity.java
+++ app/src/main/java/edu/gatech/buzzshelter/controller/LoginActivity.java
@@ -1,10 +1,17 @@
 package edu.gatech.buzzshelter.controller;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.hardware.input.InputManager;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import edu.gatech.buzzshelter.R;
 
@@ -29,7 +36,36 @@ public class LoginActivity extends AppCompatActivity
 
         /* Implement login feature */
         login.setOnClickListener(v -> {
-            /* TODO */
+            EditText userText = findViewById(R.id.userText);
+            EditText passText = findViewById(R.id.passText);
+
+            InputMethodManager manager = (InputMethodManager)
+                    getSystemService(Context.INPUT_METHOD_SERVICE);
+
+            /* Hide the 'input' virtual keyboard */
+            if(getCurrentFocus() != null && manager != null)
+            {
+                manager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+                        InputMethodManager.HIDE_NOT_ALWAYS);
+            }
+
+            /* Hardcoded success */
+            if(userText.getText().toString().equals("user")
+                    && passText.getText().toString().equals("pass"))
+            {
+                /* TODO */
+            }
+            else
+            {
+                Snackbar.make(findViewById(android.R.id.content),
+                        "Username or password incorrect", Snackbar.LENGTH_SHORT)
+                        .show();
+
+                /* Only clear out password, since that's most likely to be wrong
+                 * as the user can't visually "see" the password */
+
+                passText.setText("");
+            }
         });
     }
 
