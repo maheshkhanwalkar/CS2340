ff96db156921e7982a3014f16c8653ae2084277c
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2018-02-09 11:34:56 -0500

    Login: Implement Activity transition
    Perform an activity transition from the Welcome (Starting) Page to the
    Login Page when the 'LOGIN' button is pressed
    
    Moved the hardcoded strings into strings.xml to conform to the
    Internationalization standards

--- app/src/main/java/edu/gatech/buzzshelter/controller/WelcomeActivity.java
+++ app/src/main/java/edu/gatech/buzzshelter/controller/WelcomeActivity.java
@@ -1,8 +1,10 @@
 package edu.gatech.buzzshelter.controller;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.Button;
 
 import edu.gatech.buzzshelter.R;
 
@@ -15,6 +17,18 @@ public class WelcomeActivity extends AppCompatActivity
         setContentView(R.layout.activity_welcome);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Button login = findViewById(R.id.loginButton);
+        Button register = findViewById(R.id.registerButton);
+
+        login.setOnClickListener(v -> {
+            Intent loginIntent = new Intent(this, LoginActivity.class);
+            startActivity(loginIntent);
+        });
+
+        register.setOnClickListener(v -> {
+            /* TODO */
+        });
     }
 
 }
