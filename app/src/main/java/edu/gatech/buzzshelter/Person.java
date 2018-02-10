9e6ab27c860667f598fb3b4bc6fd8c373721cd52
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2018-02-10 16:55:50 -0500

    Revert "Added baseline Activity-related code:"
    This reverts commit b97bdac6d11aa876a572b2b291f3b66f6d30beb5.
    
    Active development should be done on the various feature branches, while
    merging and cleanup should be done on the 'staging' branch.
    
    Once the feature is "stable" enough and working within 'staging', it can
    be pushed to 'master'

--- app/src/main/java/edu/gatech/buzzshelter/Person.java
+++ app/src/main/java/edu/gatech/buzzshelter/Person.java
@@ -5,19 +5,4 @@ package edu.gatech.buzzshelter;
  */
 
 public class Person {
-    String name;
-    String username;
-    String password;
-    boolean locked;
-    String email;
-    int phone;
-
-    public Person(String name, String username, String password, String email, int phone) {
-        this.name = name;
-        this.username = username;
-        this.password = password;
-        this.locked = false;
-        this.email = email;
-        this.phone = phone;
-    }
 }
--- app/src/main/java/edu/gatech/buzzshelter/User.java
+++ app/src/main/java/edu/gatech/buzzshelter/User.java
@@ -5,7 +5,4 @@ package edu.gatech.buzzshelter;
  */
 
 public class User extends Person {
-    public User(String name, String username, String password, String email, int phone) {
-        super(name, username, password, email, phone);
-    }
 }
