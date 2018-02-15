8ffaa9a8819fe63414ccf782e22d4d80d847027c
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2018-02-15 18:20:04 -0500

    Register: Create RelativeLayout
    Switch the RegisterActivity layout from Constraint to Relative, which is
    much simpler to configure.
    
    Create onclick handlers for the 'register' and 'cancel' buttons, which
    currently have limited functionality:
    
      register: just moves to MainActivity - no actual registration
      cancel: full functionality (does what is expected)

--- app/src/main/java/edu/gatech/buzzshelter/controller/RegisterActivity.java
+++ app/src/main/java/edu/gatech/buzzshelter/controller/RegisterActivity.java
@@ -1,8 +1,10 @@
 package edu.gatech.buzzshelter.controller;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.Button;
 
 import edu.gatech.buzzshelter.R;
 
@@ -17,6 +19,21 @@ public class RegisterActivity extends AppCompatActivity
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        Button register = findViewById(R.id.registerButton);
+        Button cancel = findViewById(R.id.cancelButton);
+
+        /* Implement register */
+        /* TODO actually register */
+        register.setOnClickListener(v -> {
+            Intent back = new Intent(this, MainActivity.class);
+            startActivity(back);
+        });
+
+        /* Implement cancel */
+        cancel.setOnClickListener(v -> {
+            Intent back = new Intent(this, WelcomeActivity.class);
+            startActivity(back);
+        });
     }
 
 }
