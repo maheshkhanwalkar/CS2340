0ab49306f2f0eeab95b03dc269db1d41858774c7
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2018-02-16 17:38:08 -0500

    Model: Revise Database interface
    Added a contains(...) method to Database and modified the return type of
    put(...) to be boolean - indicating success or failure, taking into
    consideration the current DB state and the overwrite flag
    
    This Database interface change required changes to MemDB, which
    implements it, and Manager which uses it

--- app/src/main/java/edu/gatech/buzzshelter/model/control/Manager.java
+++ app/src/main/java/edu/gatech/buzzshelter/model/control/Manager.java
@@ -20,18 +20,15 @@ public class Manager
         return ourInstance;
     }
 
-    /* TODO accept user type */
     public boolean register(String name, String username, String password, String email)
     {
         User user = new User(name, username, password, email);
-        provider.put(user);
-
-        return true;
+        return provider.put(user, false);
     }
 
     public boolean login(String username, String password)
     {
         Credential cred = new Credential(username, password);
-        return provider.get(cred) != null;
+        return !provider.contains(cred);
     }
 }
--- app/src/main/java/edu/gatech/buzzshelter/model/db/MemDB.java
+++ app/src/main/java/edu/gatech/buzzshelter/model/db/MemDB.java
@@ -10,15 +10,27 @@ public class MemDB implements Database
     private HashMap<Credential, Person> db = new HashMap<>();
 
     @Override
-    public void put(Person person)
+    public boolean put(Person person, boolean overwrite)
     {
         Credential cred = person.getCred();
+
+        /* Already exists (and write disabled) */
+        if(db.containsKey(cred) && !overwrite)
+            return false;
+
         db.put(cred, person);
+        return true;
     }
 
     @Override
     public Person get(Credential cred)
     {
         return db.get(cred);
+    }
+
+    @Override
+    public boolean contains(Credential cred)
+    {
+        return db.containsKey(cred);
     }
 }
