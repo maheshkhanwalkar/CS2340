236fa4c8405e81e456b44f35e56f6f2e0bb2ff36
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2018-02-16 18:26:47 -0500

    Register: Implement Retype Password checks
    Created a Retype Password prompt that sets the retype password EditText
    error prompt when retyped-pass != orig-pass
    
    This has been tested with the current UI (commented out), but the pushed
    version has the NULL'd out (placeholder) version to allow for UI changes

--- app/src/main/java/edu/gatech/buzzshelter/controller/RegisterActivity.java
+++ app/src/main/java/edu/gatech/buzzshelter/controller/RegisterActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,6 +13,8 @@ import android.widget.Spinner;
 
 import edu.gatech.buzzshelter.R;
 import edu.gatech.buzzshelter.model.control.Manager;
+import edu.gatech.buzzshelter.model.user.Person;
+import edu.gatech.buzzshelter.model.user.PersonType;
 
 public class RegisterActivity extends AppCompatActivity
 {
@@ -25,6 +29,73 @@ public class RegisterActivity extends AppCompatActivity
         /* TODO hook up to UI */
         Button register = null;
         Button cancel = null;
+
+        Spinner types = null;
+
+        /* All the text fields */
+        EditText nameText = null;
+        EditText userText = null;
+        EditText passText = null;
+        EditText retypeText = null;
+        EditText emailText = null;
+
+        /*Button register = findViewById(R.id.registerButton);
+        Button cancel = findViewById(R.id.cancelButton);
+
+        Spinner types = findViewById(R.id.accSpinner);
+
+        /* All the text fields */
+        /*EditText nameText = findViewById(R.id.nameText);
+        EditText userText = findViewById(R.id.userText);
+        EditText passText = findViewById(R.id.passText);
+        EditText retypeText = findViewById(R.id.retypeText);
+        EditText emailText = userText;*/
+
+        /* Setup Retype Password */
+        retypeText.addTextChangedListener(new TextWatcher()
+        {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count)
+            {
+                if(!s.toString().equals(passText.getText().toString()))
+                {
+                    retypeText.setError("Passwords don't match");
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
+
+        /* Show error, if password is changed after retype pass is entered */
+        passText.addTextChangedListener(new TextWatcher()
+        {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count)
+            {
+                if(!retypeText.getText().toString().equals("")
+                        && !s.toString().equals(retypeText.getText().toString()))
+                {
+                    retypeText.setError("Passwords don't match");
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
+
+        /* Setup spinner */
+        PersonType[] people = PersonType.values();
+        ArrayAdapter<PersonType> cs = new ArrayAdapter<>(
+                this, android.R.layout.simple_spinner_item, people);
+
+        types.setAdapter(cs);
 
         /* TODO plan out register */
         register.setOnClickListener(v -> {
