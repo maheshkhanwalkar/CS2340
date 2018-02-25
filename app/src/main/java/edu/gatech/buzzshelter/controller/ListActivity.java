fed9033839a334109c0d59d1caf60b03831db866
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2018-02-25 13:03:38 -0500

    Shelter: Add parsing code
    Added parsing code that would take the source CSV file and extract the
    records, which are used to build Shelter objects and are stored within a
    list
    
    Modified Shelter's capacity field to a String, since some of the CSV
    data has Strings, not just ints

---
+++ app/src/main/java/edu/gatech/buzzshelter/controller/ListActivity.java
@@ -0,0 +1,86 @@
+package edu.gatech.buzzshelter.controller;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.io.InputStream;
+import java.util.List;
+
+import edu.gatech.buzzshelter.R;
+import edu.gatech.buzzshelter.model.control.Manager;
+import edu.gatech.buzzshelter.model.user.Shelter;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+        InputStream stream = getResources().openRawResource(R.raw.shelter);
+        Manager manager = Manager.getInstance();
+        manager.parseShelter(stream);
+
+        RecyclerView recyclerView = findViewById(R.id.shelterList);
+        recyclerView.setAdapter(new SimpleRecyclerViewAdapter(manager.getShelters()));
+    }
+
+    public class SimpleRecyclerViewAdapter extends RecyclerView.Adapter<SimpleRecyclerViewAdapter.ViewHolder> {
+        private final List<Shelter> mShelters;
+
+        public SimpleRecyclerViewAdapter(List<Shelter> shelters) {
+            mShelters = shelters;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.shelter_list_content, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            Manager manager = Manager.getInstance();
+
+            holder.mShelter = mShelters.get(position);
+            holder.mName.setText(holder.mShelter.getName());
+            holder.mContentView.setText(holder.mShelter.getNotes());
+
+            // listener?
+        }
+
+        @Override
+        public int getItemCount() {
+            return mShelters.size();
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            public final View mView;
+            public final TextView mName;
+            public final TextView mContentView;
+            public Shelter mShelter;
+
+            public ViewHolder(View view) {
+                super(view);
+                mView = view;
+                mName = view.findViewById(R.id.name);
+                mContentView = view.findViewById(R.id.content);
+            }
+
+            @Override
+            public String toString() {
+                return super.toString() + " '" + mContentView.getText() + "'";
+            }
+        }
+    }
+
+}
--- app/src/main/java/edu/gatech/buzzshelter/controller/MainActivity.java
+++ app/src/main/java/edu/gatech/buzzshelter/controller/MainActivity.java
@@ -1,12 +1,16 @@
 package edu.gatech.buzzshelter.controller;
 
+import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.Button;
 
+import java.io.InputStream;
+
 import edu.gatech.buzzshelter.R;
+import edu.gatech.buzzshelter.model.control.Manager;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -19,12 +23,19 @@ public class MainActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
 
         Button logout = findViewById(R.id.logoutButton);
+        Button read = findViewById(R.id.readButton);
 
         /* Logout implementation */
         logout.setOnClickListener(v -> {
             Intent landing = new Intent(this, WelcomeActivity.class);
             landing.putExtra("DISABLE_BACK", true);
 
+            startActivity(landing);
+        });
+
+        /* Load shelter page */
+        read.setOnClickListener(v -> {
+            Intent landing = new Intent(this, edu.gatech.buzzshelter.controller.ListActivity.class);
             startActivity(landing);
         });
     }
