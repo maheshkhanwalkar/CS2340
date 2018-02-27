6b9b25b6c8c4f26db1c2b80bb9329a567b0bfac8
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2018-02-27 14:43:00 -0500

    Shelter: Fix problem with RecyclerView
    Fixed problem with the RecyclerView not showing anything. In addition to
    setting the adapter, the layout manager must also be set. It has
    currently been set to LinearLayout, but can be modified to something
    else, if necessary

--- app/src/main/java/edu/gatech/buzzshelter/controller/ListActivity.java
+++ app/src/main/java/edu/gatech/buzzshelter/controller/ListActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
@@ -31,6 +32,9 @@ public class ListActivity extends AppCompatActivity {
 
         RecyclerView recyclerView = findViewById(R.id.shelterList);
         recyclerView.setAdapter(new SimpleRecyclerViewAdapter(manager.getShelters()));
+
+        /* FIXME: Adjust the layout to what is best */
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
     public class SimpleRecyclerViewAdapter extends RecyclerView.Adapter<SimpleRecyclerViewAdapter.ViewHolder> {
