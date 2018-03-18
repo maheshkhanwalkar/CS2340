e3331bd59715385f86eb2269b9daa317c5c8565f
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2018-03-18 15:45:29 -0400

    Login: Add Firebase authentication support
    Added support for persistant account creation and login using the
    Firebase Authentication. This replaces the previous setup, which was
    neither persistant nor full functional and flexible.
    
    The main WelcomeActivity has been stripped down and automatically pulls
    up the AuthUI fragment. The Login and Register activities still exist,
    but will be removed in the near future.

--- app/src/main/java/edu/gatech/buzzshelter/controller/WelcomeActivity.java
+++ app/src/main/java/edu/gatech/buzzshelter/controller/WelcomeActivity.java
@@ -6,32 +6,38 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.Button;
 
+import com.firebase.ui.auth.AuthUI;
+
+import java.util.Arrays;
+
 import edu.gatech.buzzshelter.R;
 
 public class WelcomeActivity extends AppCompatActivity
 {
     private boolean disableBack = false;
+    private static final int RC_SIGN_IN = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_welcome);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Button login = findViewById(R.id.loginButton);
-        Button register = findViewById(R.id.registerButton);
-
-        login.setOnClickListener(v -> {
-            Intent loginIntent = new Intent(this, LoginActivity.class);
-            startActivity(loginIntent);
-        });
-
-        register.setOnClickListener(v -> {
-            Intent registerIntent = new Intent(this, RegisterActivity.class);
-            startActivity(registerIntent);
-        });
+        /* TODO: handle result of sign-up/login */
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setIsSmartLockEnabled(false, true)
+                        .setAvailableProviders(Arrays.asList(
+                                new AuthUI.IdpConfig.EmailBuilder().build(),
+                                /* FIXME: Google OAuth has not yet been set up */
+                                new AuthUI.IdpConfig.GoogleBuilder().build()
+                        ))
+                        .build()
+                , RC_SIGN_IN);
     }
 
     @Override
